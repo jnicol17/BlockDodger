ffc7ea61c39d89962b95fc4840d28ce4673a1dd7
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-06-16 11:39:00 -0400

    Added Power up images

--- FirstGame/Assets/Scripts/GameController.cs
+++ FirstGame/Assets/Scripts/GameController.cs
@@ -62,6 +62,8 @@ public class GameController : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+        // 0 for no sync, 1 for panel refresh rate, 2 for 1/2 panel rate
+        QualitySettings.vSyncCount = 1;
     }
 
     // Use this for initialization  
